e1c67b84df88d0b63b3a94a84f076d9081e06a44
raginijetly <raginijetly98@gmail.com>
2025-07-02 23:59:04 +0530

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-const BACKEND_API_URL = "https://femfit-backend.onrender.com";
+const BACKEND_API_URL = "https://femfit-backend.onrender.com/api";
 
 const UI_DELAY = 500; // 0.5 second delay for UI transitions
 
