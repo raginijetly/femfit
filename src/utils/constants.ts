00c7dcc71359e88f16c2c538f07dd0311a5b5ae9
raginijetly <raginijetly98@gmail.com>
2025-09-14 15:25:31 +0530

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,9 @@
 const BACKEND_API_URL = "https://femfit-backend.onrender.com/api";
 // const BACKEND_API_URL = "http://localhost:3000/api";
 
+const FRONTEND_URL = "https://femfit-alpha.vercel.app";
+// const FRONTEND_URL = "http://localhost:5173";
+
 const UI_DELAY = 500; // 0.5 second delay for UI transitions
 
-export { BACKEND_API_URL, UI_DELAY };
+export { BACKEND_API_URL, UI_DELAY, FRONTEND_URL };
